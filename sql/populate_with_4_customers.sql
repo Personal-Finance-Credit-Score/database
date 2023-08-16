c3fa994e7ca9d257345b98d7b4b58706c689ffeb
BekzodTolipov <btolipov110@gmail.com>
2023-08-16 12:41:05 -0500

    Update poor dti customer

--- sql/populate_with_4_customers.sql
+++ sql/populate_with_4_customers.sql
@@ -3,26 +3,26 @@
 INSERT INTO Customer (customer_id, username, password, email)
 VALUES (1, 'santiagosuzanne', 'password', 'johnwang@yahoo.com');
 
-INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (1, 1, 23497.71, 11.88, 32, 4, 0.14, 20272.41, 0, 2, 0, 0, '2023-08-16');
-INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (2, 1, 35841.71, 14.88, 13, 4, 0.86, 5113.56, 0, 2, 0, 1, '2023-08-16');
-INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (3, 1, 26965.58, 4.51, 20, 4, 0.11, 24053.88, 0, 2, 0, 1, '2023-08-16');
-INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (4, 1, 41217.86, 11.26, 21, 1, 0.57, 17654.4, 0, 1, 1, 1, '2023-08-16');
-INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (5, 1, 6890.03, 9.68, 58, 4, 0.68, 2220.13, 0, 3, 0, 1, '2023-08-16');
-INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (6, 1, 48667.61, 2.77, 21, 3, 0.88, 5811.39, 0, 0, 0, 1, '2023-08-16');
-INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (7, 1, 41447.57, 7.36, 39, 1, 0.88, 5127.72, 0, 3, 1, 0, '2023-08-16');
-INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (8, 1, 8737.46, 12.37, 15, 2, 0.7, 2584.57, 0, 1, 2, 0, '2023-08-16');
-INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (9, 1, 39776.1, 2.11, 17, 4, 0.17, 33028.57, 0, 0, 1, 0, '2023-08-16');
-INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (10, 1, 36677.27, 12.77, 37, 2, 0.74, 9605.13, 0, 3, 1, 0, '2023-08-16');
-INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (11, 1, 5539.46, 14.15, 45, 2, 0.88, 644.56, 0, 1, 1, 1, '2023-08-16');
-INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (12, 1, 34330.56, 10.6, 18, 4, 0.97, 1142.51, 0, 0, 1, 0, '2023-08-16');
-INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (13, 1, 23564.58, 11.57, 51, 2, 0.52, 11418.43, 0, 3, 0, 1, '2023-08-16');
-INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (14, 1, 9520.98, 11.16, 50, 2, 0.29, 6725.61, 0, 2, 0, 1, '2023-08-16');
-INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (15, 1, 42932.89, 8.72, 33, 2, 0.56, 18755.66, 0, 0, 1, 0, '2023-08-16');
-INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (16, 1, 6444.85, 9.85, 44, 1, 0.53, 3036.08, 0, 3, 1, 0, '2023-08-16');
-INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (17, 1, 24725.62, 13.25, 49, 1, 0.44, 13723.6, 0, 2, 0, 0, '2023-08-16');
-INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (18, 1, 26973.72, 4.45, 25, 4, 0.1, 24276.52, 0, 3, 2, 0, '2023-08-16');
-INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (19, 1, 38785.5, 14.7, 58, 4, 0.38, 23958.75, 0, 2, 0, 0, '2023-08-16');
-INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (20, 1, 28271.21, 10.7, 59, 1, 0.15, 24055.89, 0, 1, 1, 0, '2023-08-16');
+INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (1, 1, 9241.47, 3.04, 49, 3, 0.55, 4171.38, 22, 2, 0, 0, '2021-10-25');
+INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (2, 1, 1787.15, 2.85, 19, 4, 0.61, 693.6, 7, 1, 2, 1, '2023-01-18');
+INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (3, 1, 39692.26, 12.27, 17, 1, 0.82, 7000.56, 3, 2, 2, 1, '2023-05-18');
+INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (4, 1, 2438.0, 4.69, 44, 1, 0.74, 624.94, 11, 2, 1, 1, '2022-09-20');
+INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (5, 1, 5175.55, 5.26, 45, 3, 0.98, 113.78, 1, 1, 1, 1, '2023-07-17');
+INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (6, 1, 12959.5, 4.01, 45, 1, 0.24, 9818.28, 34, 0, 1, 0, '2020-10-30');
+INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (7, 1, 25277.95, 4.2, 26, 1, 0.55, 11335.42, 12, 3, 1, 0, '2022-08-21');
+INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (8, 1, 42831.6, 10.27, 16, 4, 0.36, 27539.95, 10, 1, 1, 1, '2022-10-20');
+INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (9, 1, 46195.1, 4.41, 17, 2, 0.07, 43070.22, 16, 1, 1, 1, '2022-04-23');
+INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (10, 1, 40601.19, 6.51, 24, 4, 0.3, 28559.55, 17, 0, 1, 0, '2022-03-24');
+INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (11, 1, 27293.17, 12.45, 59, 3, 0.61, 10759.35, 23, 0, 1, 0, '2021-09-25');
+INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (12, 1, 34847.11, 6.41, 41, 4, 0.37, 22050.09, 26, 2, 1, 1, '2021-06-27');
+INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (13, 1, 10326.73, 10.0, 21, 1, 0.77, 2353.32, 5, 1, 0, 0, '2023-03-19');
+INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (14, 1, 44725.21, 12.08, 39, 4, 0.38, 27755.59, 24, 1, 0, 0, '2021-08-26');
+INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (15, 1, 35721.17, 11.91, 49, 4, 0.47, 18782.07, 26, 0, 2, 1, '2021-06-27');
+INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (16, 1, 17545.38, 8.12, 51, 2, 0.18, 14471.99, 42, 3, 2, 0, '2020-03-04');
+INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (17, 1, 7019.19, 6.38, 60, 3, 0.0, 6988.89, 60, 0, 1, 1, '2018-09-11');
+INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (18, 1, 24985.84, 2.39, 52, 3, 0.79, 5337.55, 11, 3, 1, 0, '2022-09-20');
+INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (19, 1, 6043.53, 13.56, 12, 1, 0.27, 4428.18, 9, 3, 0, 0, '2022-11-19');
+INSERT INTO Loan (loan_id, customer_id, loan_amount, interest_rate, loan_term, loan_type_id, credit_utilization_ratio, payment_history, number_of_payments, days_late_30, days_late_60, days_90_plus, date_opened) VALUES (20, 1, 29075.23, 4.82, 29, 2, 0.5, 14500.81, 14, 1, 1, 1, '2022-06-22');
 
 
 INSERT INTO PersonalDetails (customer_id, first_name, last_name, birthdate, gender_id, marital_id, education_id, employment_id, current_address_id)
@@ -42,10 +42,21 @@ VALUES
 
 -- Assuming today's date is '2023-08-03'
 -- Insert historical credit scores for customer 1 for 24 months
-INSERT INTO HistoricalCreditScores (customer_id, credit_score, timestamp)
+WITH CreditScoreData AS (
+    SELECT
+        1 AS customer_id,
+        ROUND(RANDOM() * (850 - 300) + 300) AS credit_score,
+        (CURRENT_DATE - INTERVAL '1 month' * n) AS timestamp
+    FROM
+        generate_series(0, 23) AS n
+)
+INSERT INTO HistoricalCreditScores (customer_id, credit_bureau, credit_score, timestamp)
 SELECT
-    1 AS customer_id,
-    ROUND(RANDOM() * (850 - 300) + 300) AS credit_score,
-    (CURRENT_DATE - INTERVAL '1 month' * n) AS timestamp
+    csd.customer_id,
+    bureaus.credit_bureau,
+    csd.credit_score,
+    csd.timestamp
 FROM
-    generate_series(0, 23) AS n;
+    CreditScoreData csd
+CROSS JOIN
+    (VALUES ('Transunion'), ('Equifax'), ('Experian')) AS bureaus(credit_bureau);
